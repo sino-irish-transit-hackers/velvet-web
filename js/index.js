19594e176b0e07ed9e6db9ac18318d21a9573f9e
Jesse Lee <mienaikoe@gmail.com>
2015-02-20 00:16:13 -0500

    fadeouts of up and downtowns

--- js/index.js
+++ js/index.js
@@ -4,17 +4,14 @@ var scaleFactor = 1.0;
 
 function draw(){
 	ctx.clearRect(0,0,360,240);
-	ctx.lineWidth = 12.0/Math.max(1.0, scaleFactor);
-	ctx.strokeStyle = "rgba(0,100,255,0.5)";
-	ctx.fillStyle = "white";
-	ctx.font = (10/scaleFactor) + "px sans-serif";
-	
 	
 	ctx.save();
 	
 	ctx.translate(180, 0);
 	ctx.scale(scaleFactor, scaleFactor);
 	
+	ctx.lineWidth = 10.0/Math.max(1.0, scaleFactor);
+	ctx.strokeStyle = "rgba(0,100,255,1)";
 	ctx.beginPath();
 	ctx.moveTo(0,0);
 	ctx.lineTo(0,200);
@@ -27,7 +24,31 @@ function draw(){
 	ctx.stroke();
 	ctx.closePath();
 	
-	ctx.fillText("A", -3/scaleFactor, 10);
+	ctx.lineWidth = 10.0;
+	ctx.strokeStyle = "rgba(0,100,255,"+(3/scaleFactor)+")";
+	ctx.beginPath();
+	ctx.moveTo(2,0);
+	ctx.lineTo(2,200);
+	ctx.stroke();
+	ctx.closePath();
+	
+	
+	ctx.textAlign = "center";
+	ctx.font = (10/scaleFactor) + "px sans-serif";
+	
+	ctx.fillStyle = "rgba(255,255,255,1.0)";
+	ctx.fillText("C", 2, 10);
+	
+	ctx.fillStyle = "rgba(255,255,255,"+((scaleFactor/5.0)-0.5)+")";
+	ctx.fillText("D", 0, 5);
+	ctx.fillText("D", 0, 15);
+	ctx.fillText("D", 0, 25);
+	ctx.fillText("D", 0, 35);
+	
+	ctx.fillText("U", 4, 5);
+	ctx.fillText("U", 4, 15);
+	ctx.fillText("U", 4, 25);
+	ctx.fillText("U", 4, 35);
 	
 	ctx.restore();
 }
@@ -43,11 +64,11 @@ $(document).ready(function(){
 	$(canvas).bind("mousewheel",function(ev){
 		if( ev.originalEvent.wheelDelta > 0 ){
 			if( scaleFactor < 5.0 ){
-				scaleFactor += .03;
+				scaleFactor += .05;
 			}
 		} else if( ev.originalEvent.wheelDelta < 1 ){
-			if( scaleFactor > 0.1){
-				scaleFactor -= .03;
+			if( scaleFactor > 0.5){
+				scaleFactor -= .05;
 			}
 		}
 		draw();
